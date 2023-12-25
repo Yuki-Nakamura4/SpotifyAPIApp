c6a7293eabb6647f694ee0e78bc80dbd15ed006a
Yuki-Nakamura4 <Yuki.Nakamura4@jp.ricoh.com>
2023-12-25 23:35:02 +0900

    設定した色をlocalstorageに保存して読み込めるように変更

--- src/Components/pages/Settings.tsx
+++ src/Components/pages/Settings.tsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import {keysColor} from '../../data/KeysColor';
+import { keysColor } from '../../data/KeysColor';
 
 export const Settings = () => {
   const storedColors = localStorage.getItem('userColors');
-  const defaultColors = keysColor.map(key => key.color);
-  const initialColors = storedColors ? JSON.parse(storedColors) : defaultColors;
-  const [usersColors, setUsersColors] = useState<string[]>(initialColors);
+  const initialColors = storedColors
+    ? JSON.parse(storedColors)
+    : keysColor.reduce((colors, key) => ({ ...colors, [key.name]: key.color }), {});
+  const [usersColors, setUsersColors] = useState<{ [key: string]: string }>(initialColors);
 
   useEffect(() => {
     localStorage.setItem('userColors', JSON.stringify(usersColors));
   }, [usersColors]);
 
-  const handleColorChange = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newColors = [...usersColors];
-    newColors[index] = event.target.value;
+  const handleColorChange = (keyName: string) => (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const newColors = { ...usersColors, [keyName]: event.target.value };
     setUsersColors(newColors);
+    localStorage.setItem('userColors', JSON.stringify(newColors));
   };
 
   return (
@@ -22,12 +25,12 @@ export const Settings = () => {
       <div className="p-6">・キーの色</div>
       <div>※この機能は未完成です。</div>
       <div className="pl-6">
-      {usersColors.map((color, index) => (
-        <div key={index}>
-          <label>{keysColor[index].name}:</label>
-          <input type="color" value={color} onChange={handleColorChange(index)} />
-        </div>
-      ))}
+        {keysColor.map((key) => (
+          <div key={key.name}>
+            <label>{key.name}:</label>
+            <input type="color" value={usersColors[key.name]} onChange={handleColorChange(key.name)} />
+          </div>
+        ))}
       </div>
     </>
   );
--- src/data/KeysColor.ts
+++ src/data/KeysColor.ts
@@ -2,24 +2,29 @@ export type KeysColor = {
     name: string;
     color: string;
   };
-
-export type usersColor = {
-    color: string;
+  
+  export type UsersColor = {
+    [key: string]: string;
   };
+  
+  const storedUsersColor: UsersColor = JSON.parse(localStorage.getItem('userColors') || '{}');
 
-const usersColor: usersColor[] =[]
-
-export const keysColor: KeysColor[] = [
-    { name: "C/Am", color: '#fff8ff'},
-    { name: "G/Em", color: '#00CEFF'},
-    { name: "D/Bm", color: '#FFFF8A'},
-    { name: "A/F♯m", color: '#FF596D'},
-    { name: "E/C♯m", color: '#00FF7E' },
-    { name: "B/G♯m", color: '#FFC88A'},
-    { name: "G♭/D♯m", color: '#BB62FF'},
-    { name: "D♭/B♭m", color: '#D4FFF6'},
-    { name: "A♭/Fm", color: '#4F5EFF'},
-    { name: "E♭/Cm", color: '#00B5D4'},
-    { name: "B♭/Gm", color: '#ECEBEC'},
-    { name: "F/Dm", color: '#FFF8D8'},
+  export const keysColor: KeysColor[] = [
+    { name: "C/Am", color: storedUsersColor["C/Am"] || '#fff8ff' },
+    { name: "G/Em", color: storedUsersColor["G/Em"] || '#00CEFF' },
+    { name: "D/Bm", color: storedUsersColor["D/Bm"] || '#FFFF8A' },
+    { name: "A/F♯m", color: storedUsersColor["A/F♯m"] || '#FF596D' },
+    { name: "E/C♯m", color: storedUsersColor["E/C♯m"] || '#00FF7E' },
+    { name: "B/G♯m", color: storedUsersColor["B/G♯m"] || '#FFC88A' },
+    { name: "G♭/D♯m", color: storedUsersColor["G♭/D♯m"] || '#BB62FF' },
+    { name: "D♭/B♭m", color: storedUsersColor["D♭/B♭m"] || '#D4FFF6' },
+    { name: "A♭/Fm", color: storedUsersColor["A♭/Fm"] || '#4F5EFF' },
+    { name: "E♭/Cm", color: storedUsersColor["E♭/Cm"] || '#00B5D4' },
+    { name: "B♭/Gm", color: storedUsersColor["B♭/Gm"] || '#ECEBEC' },
+    { name: "F/Dm", color: storedUsersColor["F/Dm"] || '#FFF8D8' },
   ];
+  
+  export const updateLocalStorage = (newColors: UsersColor) => {
+    localStorage.setItem('userColors', JSON.stringify(newColors));
+  };
+  
