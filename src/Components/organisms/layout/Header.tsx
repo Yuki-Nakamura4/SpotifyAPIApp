14c8485c0a4e400316a3bf1ca9d00fc1b71f5764
Yuki-Nakamura4 <Yuki.Nakamura4@jp.ricoh.com>
2023-12-25 23:56:44 +0900

    マイページをヘッダーから削除

--- src/Components/organisms/layout/Header.tsx
+++ src/Components/organisms/layout/Header.tsx
@@ -31,7 +31,7 @@ export const Header = memo(() => {
     </div>
       <div className="mt-5 mb-4 flex justify-center space-x-8">
       <p className={`text-lg ${isTopPage ? 'text-red-400 border-b border-red-400' : 'text-slate-500'} hover:text-red-400  invisible sm:visible font-Quicksand font-600 cursor-pointer`} onClick={onClickTop}>TOP</p>
-      <p className={`text-lg ${isMyPage ? 'text-red-400 border-b border-red-400' : 'text-slate-500'} hover:text-red-400  invisible sm:visible font-Quicksand font-600 cursor-pointer`} onClick={onClickMypage}>MY PAGE</p>
+      {/* <p className={`text-lg ${isMyPage ? 'text-red-400 border-b border-red-400' : 'text-slate-500'} hover:text-red-400  invisible sm:visible font-Quicksand font-600 cursor-pointer`} onClick={onClickMypage}>MY PAGE</p> */}
       <p className={`text-lg ${isQuiz ? 'text-red-400 border-b border-red-400' : 'text-slate-500'} hover:text-red-400  invisible sm:visible font-Quicksand font-600 cursor-pointer`} onClick={onClickQuiz}>QUIZ</p>
       <p className={`text-lg ${isSettings ? 'text-red-400 border-b border-red-400' : 'text-slate-500'} hover:text-red-400  invisible sm:visible font-Quicksand font-600 cursor-pointer`} onClick={onClickSettings}>SETTINGS</p>
       </div>
